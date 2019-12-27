1493591105b54073c6b50584027f154b7a92856a
varianwrynn <13040839537@163.com>
2019-12-28 00:25:38 +0800

    添加基本语法模块。

--- src/com/cornucopia/kotlin/start/base/index.kt
+++ src/com/cornucopia/kotlin/start/base/index.kt
@@ -1,16 +1,17 @@
 package com.cornucopia.kotlin.start.base
 
 /**
- *
+ * kotlin-基本语法
  * @author cornucopia
  * @version 1.0
  * @since 2019/12/28
  */
 fun main() {
-
+    var sum = sum(1, 2)
+    print(sum)
 }
 
 //1.函数
-fun sum(a:Int,b:Int):Int{
-    return a+b
+fun sum(a: Int, b: Int): Int {
+    return a + b
 }
