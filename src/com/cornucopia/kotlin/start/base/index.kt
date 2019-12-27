51c831597a8042e617d82f6740799d7953e37c12
varianwrynn <13040839537@163.com>
2019-12-28 00:18:40 +0800

    first commit

---
+++ src/com/cornucopia/kotlin/start/base/index.kt
@@ -0,0 +1,16 @@
+package com.cornucopia.kotlin.start.base
+
+/**
+ *
+ * @author cornucopia
+ * @version 1.0
+ * @since 2019/12/28
+ */
+fun main() {
+
+}
+
+//1.函数
+fun sum(a:Int,b:Int):Int{
+    return a+b
+}
