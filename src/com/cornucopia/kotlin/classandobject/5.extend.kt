f05725ff991061d6bbc5f835a515058ba1815cc9
varianwrynn <13040839537@163.com>
2020-01-03 21:56:59 +0800

    添加扩展函数的学习、

--- src/com/cornucopia/kotlin/classandobject/5.extend.kt
+++ src/com/cornucopia/kotlin/classandobject/5.extend.kt
@@ -7,7 +7,7 @@ package com.cornucopia.kotlin.classandobject
  * @since 2020/1/2
  */
 fun main() {
-    testShape5()
+    MyClass.printCompanion()
 }
 
 //kotlin能够扩展一个类的新功能而无需继承该类或者使用装饰者这样的设计模式。可以为一个你不能修改的，来自第三方库
@@ -49,3 +49,35 @@ fun Shape5.printFunctionType(){
 fun testShape5(){
     Shape5().printFunctionType()
 }
+//3. 可空接收者
+//3.1 注意可以为可空的接收者类型定义扩展。这样的扩展可以在对象变量上调用，即使其值为null，并且可以在函数体内检测
+fun Any?.toString():String{
+    if(this==null){
+        return "null"
+    }
+    return toString()
+}
+
+//4. 扩展属性
+//4.1 扩展没有实际的将成员插入类中，因此对扩展属性来说幕后字段是无效的。
+//4.2 因为扩展没有实际的将成员插入类中，不能有初始化器。
+val <T> List<T>.lastIndex:Int
+    get() = size-1
+
+//5. 伴生对象的扩展
+//5.1 一个类定义了伴生对象，你也可以为伴生对象定义扩展或者属性。就像伴生对象的常规成员一样,使用类名作为限定符
+//调用伴生对象的扩展成员。
+class MyClass{
+    companion object{
+    }
+}
+fun MyClass.Companion.printCompanion(){
+    println("companion")
+}
+
+//6. 扩展的作用域
+//6.1 包，在包里顶层定义扩展,包内全部可以使用;在包外，需要在调用方导入它。
+
+//7. 扩展声明为成员
+
+
